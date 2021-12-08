d58602970bc9e65f36068fb38ac6855b29707f0d
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-12-08 12:36:36 +0530

    Initial commit

---
+++ lib/test_flight.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "test_flight/version"
+
+module TestFlight
+  class Error < StandardError; end
+  # Your code goes here...
+  def self.hello
+    puts "Hello, Welcome to Test Flight gem!"
+  end
+end
---
+++ lib/test_flight/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TestFlight
+  VERSION = "0.1.0"
+end
