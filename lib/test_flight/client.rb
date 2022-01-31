b7758da1cdc2372c788415b4746ee40d9ab9ee31
Jay Bhoyar <jaybhoyar1997@gmail.com>
2022-01-31 19:32:01 +0530

    URL Test

--- lib/test_flight/client.rb
+++ lib/test_flight/client.rb
@@ -3,13 +3,23 @@
 module TestFlight
   class Client
     BASE_URL = "http://localhost:9030/api/v1"
+    STAGING_URL = "https://neeto-notifications-staging.herokuapp.com/api/v1"
 
-    attr_reader :api_key
+    attr_reader :api_key, :api_url
 
     def initialize(api_key)
-      raise "API Key is required" unless api_key
-
       @api_key = api_key
+      @api_url = pn_api_url
+    end
+
+    def pn_api_url
+      if (ENV["RAILS_ENV"] == "development") || (ENV["RAILS_ENV"] == "staging")
+        puts "#{ENV['RAILS_ENV']}"
+        BASE_URL
+      elsif ENV["RAILS_ENV"] == "production"
+        puts ENV["RAILS_ENV"]
+        STAGING_URL
+      end
     end
 
     def notification
@@ -17,7 +27,7 @@ module TestFlight
     end
 
     def connection
-      @connection ||= Faraday.new(BASE_URL) do |conn|
+      @connection ||= Faraday.new(api_url) do |conn|
         conn.adapter Faraday.default_adapter
         conn.headers["X-Api-Key"] = api_key if api_key.present?
         conn.request :json
